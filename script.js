5492f4db9aa1d632ef369b3b891b5d2f061a9097
Christian Aquino <cjfaquino@gmail.com>
2022-09-15 22:39:54 -0700

    add checkWin() logic

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const gameBoard = (() => {
     const board = ['','','','','','','','',''];
+    let gameWon = false;
     let turn = 'x'
 
     //cache DOM
@@ -8,7 +9,8 @@ const gameBoard = (() => {
 
     //bind events
     squares.forEach(el => {
-        el.addEventListener('click', addMark)
+        el.addEventListener('click', addMark);
+        el.addEventListener('click', checkWin)
     })
 
 
@@ -24,15 +26,35 @@ const gameBoard = (() => {
 
     function addMark(){
         const index = this.dataset.key;
-        if(turn=='x' && !board[index]){
+        if(turn=='x' && !board[index] && !gameWon){
             board[index] = 'x'
             turn = 'o'
-        } else if(turn=='o' && !board[index]){
+        } else if(turn=='o' && !board[index] && !gameWon){
             board[index] = 'o'
             turn = 'x'
         }
         render();
     }
 
-    return {board}
+    function checkWin(){
+        if(//rows
+            board[0] == board[1] && board[0] == board[2] && board[0] ||
+            board[3] == board[4] && board[3] == board[5] && board[3] ||
+            board[6] == board[7] && board[6] == board[8] && board[6]){
+                gameWon = true;
+            } 
+        else if(//columns
+            board[0] == board[3] && board[0] == board[6] && board[0] ||
+            board[1] == board[4] && board[1] == board[7] && board[1] ||
+            board[2] == board[5] && board[2] == board[8] && board[2]){
+                gameWon = true;
+            }
+        else if(//diagonal
+            board[0] == board[4] && board[0] == board[8] && board[0] ||
+            board[6] == board[4] && board[6] == board[2] && board[6]){
+                gameWon = true;
+        }
+    }
+
+    return {board, gameWon}
 })(); 
