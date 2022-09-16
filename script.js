ca71d2821a7b299af57557b5628b7b676c172f48
Christian Aquino <cjfaquino@gmail.com>
2022-09-15 19:01:14 -0700

    add render() with temp data

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 const gameBoard = (() => {
-    const board = ['x','x','x','o','o','o','x','x','x']
+    const board = ['x','x','x','o','o','o','x','x','x'];
+
+    //cache DOM
+    const table = document.querySelector('#table');
+
+    render();
+
+    function render() {
+        board.forEach(el => {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.textContent = el;
+            table.appendChild(square);
+        });
+    }
     return {board}
 })();
